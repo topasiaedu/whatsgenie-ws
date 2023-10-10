066c88420d34d367cdfb1d1add72372221c59c67
Stanley Ng <sngg0055@student.monash.edu>
2023-10-10 10:40:06 +0800

    Added Debug Mode for both side

--- socketServer.js
+++ socketServer.js
@@ -2,51 +2,37 @@ const socketIO = require('socket.io');
 const axios = require('axios');
 
 module.exports = (server) => {
-  // Create a new instance of the Socket.io server
   const io = socketIO(server, {
     path: '/socket.io',
     cors: {
-      origin: "*", // Allow all origins
+      origin: "*",
     }
   });
-  
 
-  // Listen for a new connection
   io.on('connection', (socket) => {
-    // Log the unique ID of the new client
     console.log(`New client connected with id ${socket.id}`);
+    socket.emit('debug', `New client connected with id ${socket.id}`);
 
-    // Initialize the hasRegistered property for the new client
     socket.hasRegistered = false;
-
-    // Initialize the userDetails property for the new client
     socket.userDetails = null;
 
-    // Listen for the 'registered' event
     socket.on('registered', () => {
-      // Log the unique ID of the registered client
       console.log(`Client with id ${socket.id} is registered.`);
-
-      // Set the hasRegistered property to true when the client registers
+      socket.emit('debug', `Client with id ${socket.id} is registered.`);
       socket.hasRegistered = true;
     });
 
-    // Listen for the 'userDetails' event
     socket.on('userDetails', (userDetails) => {
-      // Store the userDetails on the socket
-      // userDetails should be an object with phoneNumber and name properties
       socket.userDetails = userDetails;
+      socket.emit('debug', `Received userDetails: ${JSON.stringify(userDetails)}`);
     });
 
-    // Listen for the 'disconnect' event
     socket.on('disconnect', () => {
-      // Log the unique ID of the disconnected client
       console.log(`Client with id ${socket.id} disconnected`);
+      socket.broadcast.emit('debug', `Client with id ${socket.id} disconnected`);
 
-      // After a delay, check if the client has registered
       setTimeout(async () => {
         if (!socket.hasRegistered) {
-          // If the client did not register, log their unique ID and send an API request
           console.log(`Client with id ${socket.id} did not register. Perform the action here.`);
           try {
             const payload = {
@@ -54,20 +40,17 @@ module.exports = (server) => {
               caption: socket.userDetails ? socket.userDetails.caption : null,
             };
 
-            // Send a POST request to the specified URL with the payload
             await axios.post(`https://api.whatsgenie.com/send_message?access_token=${socket.userDetails ? socket.userDetails.access_token : ""}&instance_id=${socket.userDetails ? socket.userDetails.instance_id : ""}`, payload);
           } catch (error) {
-            // If the API request fails, log the error
             console.error('API Request Error:', error);
           }
         }
-      }, 10000); // The delay is 10 seconds
+      }, 10000);
     });
 
-    // Listen for the 'error' event
     socket.on('error', (error) => {
-      // If an error occurs, log the error
       console.error('Socket error:', error);
+      socket.emit('debug', `Socket error: ${error}`);
     });
   });
 };
